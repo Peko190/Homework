d6926d3f831ae913379e8e0d924d710b5f533639
Peko190 <preslav0602@gmail.com>
2025-12-01 23:11:39 +0200

    Add files via upload

---
+++ StudentJsonTxt/Repositories/StudentRepositoryTxt.cs
@@ -0,0 +1,53 @@
+﻿using StudentJsonTxt.Models;
+using System.Collections.Generic;
+
+namespace StudentJsonTxt.Repositories
+{
+    public class StudentRepositoryTxt
+    {
+        private readonly string _filePath;
+        public StudentRepositoryTxt(string filePath = "students.txt")
+        {
+            _filePath = filePath;
+        }
+
+        public void Save(List<Student> students)
+        {
+            var lines = new List<string>();
+            foreach (var s in students)
+            {
+                lines.Add($"{s.Name};{s.Grade}");
+            }
+            File.AppendAllLines(_filePath, lines);  // Дописва редове към файл
+           // File.WriteAllLines(_filePath, lines); // Презаписва целия файл
+        }
+
+        public List<Student> Load()
+        {
+            var list = new List<Student>();
+
+            if (!File.Exists(_filePath))
+                return list;
+
+            var lines = File.ReadAllLines(_filePath);
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                var parts = line.Split(';');
+
+                string name = parts[0];
+                if (!int.TryParse(parts[1], out int grade))
+                    continue;
+
+                var student = new Student(name, grade);
+
+                list.Add(student);
+            }
+
+            return list;
+        }
+    }
+}
